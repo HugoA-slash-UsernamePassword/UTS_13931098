f29cf7f9b8894160ffa35ae6c5b486eccaeae9f7
H A <hugoa3520@gmail.com>
2020-08-23 22:59:34 +1000

    full combo

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     int frameCounter; //AKA Time.frameCount;
+    int blueMax;
     public Renderer rend;
     void Start()
     {
         frameCounter = 0;
+        blueMax = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(this.name +" : "+ frameCounter);
+        if (gameObject.CompareTag("Red") && frameCounter == 100) gameObject.SetActive(false);
+        if (gameObject.CompareTag("Blue") && frameCounter == blueMax) rend.enabled = false;
         frameCounter++;
     }
 }
